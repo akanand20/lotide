ae1da5ddb8f8dd4aa8c7471585c89d504800a56e
Ankit Anand <akanand.ca@gmail.com>
2020-07-24 21:42:38 +0000

    final code done

--- countLetters.js
+++ countLetters.js
@@ -1,40 +1,13 @@
-// // FUNCTION IMPLEMENTATION
-// //const assertEqual = function(actual, expected) {
-//   if(actual === expected) {
-//     console.log("Assertion Passed: " + actual + "===" + expected);
-//     // console.log(`Assertion Passed: "${actual}" === "${expected}"`); 
-//   } else {
-//     console.log("Assertion Failed: " + actual + "!==" + expected);
-//     // console.log(`Assertion Failed: "${actual}" == "${expected}"`);
-//   } 
-// };
-
-// assertEqual("Lighthouse Labs", "Bootcamp");
-// assertEqual(1, 1);
-
-
-
-
-
-// const countof = function (string){
-//   let newArray = string.split('');
-//   //console.log(newArray)
-// let object = []
-//   for (let i = 0; i < newArray.length; i++) {
-//     if (newArray[i] === newArray[i+1]){
-//       object =+1 
-//     }
-//   console.log(object)
-//   }
-// }
-const countof = function (string){
-  for (let eachLetter of string){
-    if (eachLetter = eachLetter){
-      newArray =+1
+const countLetter = function (string){
+  const objResult = {};
+  for (i = 0; i < string.length; i++)
+  { const key  = string[i]
+    if (!objResult[key]){
+      objResult[key] = 0
     }
-    console.log(newArray)
-  } 
+    objResult[key]++
+  }
+console.log(objResult)
 }
-    
 
-countof('LHL')
+countLetter('ANAND')

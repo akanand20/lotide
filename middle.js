f4bbf7a6f55b4f2f5e7a915012df38ddb6ad2f27
Ankit Anand <akanand.ca@gmail.com>
2020-07-24 04:46:01 +0000

    final

--- middle.js
+++ middle.js
@@ -2,3 +2,27 @@
 //For arrays with odd number of elements, an array containing a single middle element should be returned. [1,2,3]- [2], length 
 //For arrays with an even number of elements, an array containing the two elements in the middle should be returned [1,2,3,4]- [2,3]
 //We can and should use the assertArraysEqual function for testing our new function. That's why we brought it in here to begin with.
+const middle = function (array){
+  let newArray = [];
+    if (array.length === 1) {
+    console.log(newArray)
+    }
+    else if (array.length  === 2) {
+     console.log(newArray)
+    }
+    else if (array.length % 2 === 0) {
+      newArray = [array[(array.length/2)-1]]
+        newArray.push(array[(array.length)/2])
+          console.log(newArray)
+    }
+    else if (array.length % 2 !== 0) {
+      newArray = [array[(array.length-1)/2]]
+        console.log(newArray)
+    }
+}
+middle([1]) // => []
+middle([1, 2]) // => []
+middle([1, 2, 3]) // => [2]
+middle([1, 2, 3, 4, 5]) // => [3]
+middle([1, 2, 3, 4]) // => [2, 3]
+middle([1, 2, 3, 4, 5, 6]) // => [3, 4]
